311dda9f7bbd89748c4f59ad27c044e35d84d49a
Juan Spada <spadajuan94@gmail.com>
2021-12-09 21:01:40 -0300

    new shuffle

--- public/countdown.js
+++ public/countdown.js
@@ -1,54 +1,49 @@
 // Set the date we're counting down to
-var countDownDate = new Date("12/07/2021 15:00:00 EST").getTime();
+var countDownDate = new Date("12/13/2021 13:00:00 EST").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
 
-// Get today's date and time
-var now = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-// Find the distance between now and the count down date
-var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// Time calculations for days, hours, minutes and seconds
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
 
-// Display the result in the element with id="demo"
-document.getElementById("days").innerText = days
-document.getElementById("hours").innerText = hours
-document.getElementById("minutes").innerText = minutes
-document.getElementById("seconds").innerText = seconds
-
-// If the count down is finished, write some text
-const showWinners = false
-if (distance < 0) {
-    if(document.querySelector('.button_connect')){
-        // document.querySelector('.button_connect').disabled = true;
+  if (distance < 0) {
+    if (document.querySelector(".button_connect")) {
+      // document.querySelector('.button_connect').disabled = true;
     }
-    if(document.querySelector('.message_active')){
-        document.querySelector('.message_active').style.display = 'none';
+    if (document.querySelector(".message_active")) {
+      document.querySelector(".message_active").style.display = "none";
     }
-    if(document.querySelector('.message_inactive')){
-        document.querySelector('.message_inactive').style.display = 'block';
+    if (document.querySelector(".message_inactive")) {
+      document.querySelector(".message_inactive").style.display = "block";
     }
-    if(!showWinners){
+    if (document.getElementById("disclaimer_btn")) {
+      document.getElementById("disclaimer_btn").style.display = "none";
     }
-    if(document.getElementById('disclaimer_btn')){
-        document.getElementById('disclaimer_btn').style.display = 'none'
+    if (document.querySelector(".shuffle_close_section")) {
+      document.querySelector(".shuffle_close_section").style.display = "flex";
     }
-    if(document.querySelector('.shuffle_close_section')){
-        document.querySelector('.shuffle_close_section').style.display = 'flex'
-    }
-    if(document.querySelector('.message-section')){
-        document.querySelector('.message-section').style.display = 'none'
+    if (document.querySelector(".message-section")) {
+      document.querySelector(".message-section").style.display = "none";
     }
     clearInterval(x);
-    document.getElementById("days").innerText = '0'
-    document.getElementById("hours").innerText = '0'
-    document.getElementById("minutes").innerText = '0'
-    document.getElementById("seconds").innerText = '0'
-}
+    document.getElementById("days").innerText = "0";
+    document.getElementById("hours").innerText = "0";
+    document.getElementById("minutes").innerText = "0";
+    document.getElementById("seconds").innerText = "0";
+  }
 }, 1000);
--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,85 @@
-const express = require('express')
-const router = express.Router()
-const User = require('./../models/user')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
-const fs = require('fs')
-
+const express = require("express");
+const router = express.Router();
+const User = require("./../models/user");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fs = require("fs");
 
 router.use(cookieParser());
 // VARIABLE DE SHOW WINNERS PARA ACTIVAR ESO
-const showWinners = true
+const showWinners = true;
 
-router.get('/', async (req, res) => {
-    // res.clearCookie("participated");
-    res.setHeader('Access-Control-Allow-Headers', '*');
+router.get("/", async (req, res) => {
+  res.clearCookie("participated");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
-    console.log(req.cookies)
-    const countUsers = await User.countDocuments();
+  console.log(req.cookies);
+  const countUsers = await User.countDocuments();
 
-    const query = {walletId: req.cookies.participated}
-    const user = await User.findOne(query);
+  const query = { walletId: req.cookies.participated };
+  const user = await User.findOne(query);
 
-    //si participaste
-    if('participated' in req.cookies){
-        // si hay user quiere decir que participaste y ganaste
-        if(user){
-            if ('participated' in req.cookies && user['winner']){
-                console.log('participo y es winner')
-                res.render('index', {participated: 1, amountParticipated: countUsers, winner: true, showWinners, walletId: req.cookies.participated})
-            }
-            else{
-                console.log('participo y no es winner')
-                res.render('index', {participated: 0, amountParticipated: countUsers, winner: false, showWinners, walletId: req.cookies.participated})
-            }
-        }
-    }else{
-        //si no participaste
-        res.render('index', {participated: 3, amountParticipated: countUsers, winner: false, showWinners, walletId: req.cookies.participated})
+  //si participaste
+  if ("participated" in req.cookies) {
+    // si hay user quiere decir que participaste y ganaste
+    if (user) {
+      if ("participated" in req.cookies && user["winner"]) {
+        console.log("participo y es winner");
+        res.render("index", {
+          participated: 1,
+          amountParticipated: countUsers,
+          winner: true,
+          showWinners,
+          walletId: req.cookies.participated,
+        });
+      } else {
+        console.log("participo y no es winner");
+        res.render("index", {
+          participated: 0,
+          amountParticipated: countUsers,
+          winner: false,
+          showWinners,
+          walletId: req.cookies.participated,
+        });
+      }
     }
-})
-router.post('/', async (req, res) => {
-    let user = new User({
-        walletId: req.body.walletId,
-        participo: 1,
-        winner: false,
-        createdAt: Date.now()
-    })
-    const users = await User.find({});
-    try{
-        // res.cookie('participated', req.body.walletId, { maxAge: 60*60*24*7  }); //3 min, esta en miliseconds
-        res.cookie('participated' , req.body.walletId, {expire : new Date() + 9999});
-        res.send('Cookie have been saved successfully');
-        const content = req.body.walletId + "\n"
-        fs.appendFile('participants.log', content, err => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        //done!
-        })
-        user = await user.save()
-    } catch(e){
-        console.log(e)
-    }
-})
+  } else {
+    //si no participaste
+    res.render("index", {
+      participated: 3,
+      amountParticipated: countUsers,
+      winner: false,
+      showWinners,
+      walletId: req.cookies.participated,
+    });
+  }
+});
+router.post("/", async (req, res) => {
+  let user = new User({
+    walletId: req.body.walletId,
+    participo: 1,
+    winner: false,
+    createdAt: Date.now(),
+  });
+  const users = await User.find({});
+  try {
+    // res.cookie('participated', req.body.walletId, { maxAge: 60*60*24*7  }); //3 min, esta en miliseconds
+    res.cookie("participated", req.body.walletId, {
+      expire: new Date() + 9999,
+    });
+    res.send("Cookie have been saved successfully");
+    const content = req.body.walletId + "\n";
+    fs.appendFile("participants.log", content, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      //done!
+    });
+    user = await user.save();
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-module.exports = router
+module.exports = router;
