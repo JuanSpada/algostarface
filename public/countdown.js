d126cd8d343acedcdfe1fdb8ff58e3078c008750
Juan Spada <spadajuan94@gmail.com>
2021-12-17 00:56:28 -0300

    shuffle closed

--- public/countdown.js
+++ public/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("12/14/2021 16:00:00 EST").getTime();
+var countDownDate = new Date("12/20/2021 16:00:00 EST").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
